b04afa184e38c72ed660f0aaa77cf0f9cf60ae75
Morteza Mousavi <mousavi310@gmail.com>
2019-01-24 11:12:46 +0330

    Add consumers.

--- src/Prometheus.MetricsExample.MyConsumer/Program.cs
+++ src/Prometheus.MetricsExample.MyConsumer/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
 
 namespace Prometheus.MetricsExample.MyConsumer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task.Run(() => ConsumeMessages("my-consumer-group-1", "my-topic-1", 2000));
+            Task.Run(() => ConsumeMessages("my-consumer-group-2", "my-topic-1", 5000));
+            Console.ReadLine();
+        }
+
+        public static async Task ConsumeMessages(string groupId, string topicName, int delayInMilliseconds)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = groupId,
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetResetType.Earliest
+            };
+
+            using (var c = new Consumer<Ignore, string>(conf))
+            {
+                c.Subscribe(topicName);
+
+                bool consuming = true;
+                c.OnError += (_, e) => consuming = !e.IsFatal;
+
+                while (consuming)
+                {
+                    try
+                    {
+                        var cr = c.Consume();
+                        Thread.Sleep(delayInMilliseconds);
+                        Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+                c.Close();
+            }
         }
     }
 }
