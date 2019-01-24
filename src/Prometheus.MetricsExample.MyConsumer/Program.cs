373679f7b1f96648d4478ca96cbac6c87796feb2
Morteza Mousavi <mousavi310@gmail.com>
2019-01-24 10:46:06 +0330

    first commit

---
+++ src/Prometheus.MetricsExample.MyConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prometheus.MetricsExample.MyConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
